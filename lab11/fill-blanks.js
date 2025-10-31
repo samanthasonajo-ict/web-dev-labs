6010bb7cd622e087211a6f1e17bb921da85c82cb
Samantha Sonajo <samanthasonajo22@gmail.com>
2025-10-31 18:29:57 +0800

    lab13,html

--- lab11/fill-blanks.js
+++ lab11/fill-blanks.js
@@ -1,3 +1,5 @@
-let name = "Samantha Sonajo";
-let age = 21;
-console.log("Hi, my name is " + name + " and I am " + age + " years old.");
+function showMessage() {
+  let name = "Samantha";
+  let age = 20;
+  console.log("Hi, my name is " + name + " and I am " + age + " years old.");
+}
